712c0cc72cebd287ed7f380dfde7fd1efa66fd9f
Dylan Avery <fossilfuelmusic@gmail.com>
2017-05-19 12:42:31 -0600

    Getting a 405 when trying to fetch from poliniex, works well in browser though

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React, { Component } from 'react';
 
 import './App.css';
 
-//SHOW AVERAGE, LAST, AND PRICE TO BUY AT, IF LAST HITS BUY SEND NOTIFICATION?
-
-
+// SHOW AVERAGE, LAST, AND PRICE TO BUY AT, IF LAST HITS BUY SEND NOTIFICATION?
+// 1 Litecoin worth 0.01407 bitcoin , 26.45 usd
+// 1 Ether wirth  0.06049 bitcon , 114.2 usd
+// 1 Dash worth 0.050 bitcoin, 94.24 usd
+// USDT_LTC , USDT_ETH, USDT_DASH on poloniex , I get a 405, too many requests? 
+//
 
 class App extends Component {
   constructor() {
@@ -16,20 +19,26 @@ class App extends Component {
 
   componentDidMount() {
     // this.fetchLite('https://btc-e.com/api/2/ltc_usd/ticker')
+    // this.fetchLite('https://btc-e.com/api/2/eth_usd/ticker')
+    // this.fetchLite('https://poloniex.com/public?command=returnTicker')
+    // this.fetchLite()
   }
 
   fetchLite() {
-    fetch('https://btc-e.com/api/2/ltc_usd/ticker', {
+    fetch('https://btc-e.com/api/2/dsh_usd/ticker', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': '*',
         'Access-Control-Allow-Methods': '*'
       },
+      strictSSL: false,
     })
     .then(response => response.json())
     .then(data => console.log(data))
+    .catch(error => console.log(error))
   }
 
   render() {
