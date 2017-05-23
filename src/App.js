7a8ed1d0d15ec0e601a40e91551c5e8b63836039
Dylan Avery <fossilfuelmusic@gmail.com>
2017-05-23 11:07:22 -0600

    Need to set higher gas limit for test TX

--- src/App.js
+++ src/App.js
@@ -1,19 +1,56 @@
 import React, { Component } from 'react';
+import Web3 from 'web3';
 import logo from './ducktalesgold.png';
 import './App.css';
+
+
+const ETHEREUM_CLIENT = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+
+const reportContractABI = [{"constant":true,"inputs":[],"name":"getReports","outputs":[{"name":"","type":"bytes32[]"},{"name":"","type":"uint256[]"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_report","type":"bytes32"},{"name":"_date","type":"uint256"}],"name":"saveReport","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"reports","outputs":[{"name":"report","type":"bytes32"},{"name":"date","type":"uint256"}],"payable":false,"type":"function"}]
+const reportContractAddress = '0x4963b143970d82e7f1cd5cf453e15fd91703e6c8'
+
+// Huey : '1EABigBsmaGhT9z7GRSxqT9gUit6Gysuii'
+// Duey : '13QPiJc4XXowfJBHGo8rQxfqGifLfsWZmj'
+// Luey : '1JsgqZ2ne7D2KYmXTwXvbApQNxXarfm9Lx'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
       payload: {},
+      reportArray: {},
+      coinReport: ETHEREUM_CLIENT.eth.contract(reportContractABI).at(reportContractAddress),
+      loadedReports: false,
     }
+  }
+
+  componentDidMount() {
+    ETHEREUM_CLIENT.eth.defaultAccount = ETHEREUM_CLIENT.eth.accounts[0]
   }
 
   fetchFromMongo() {
     fetch('/api/pricing')
     .then(response => response.json())
     .then(data => this.setState({ payload: data[data.length - 1] }))
+  }
+
+  addReport() {
+    this.state.coinReport.saveReport(JSON.stringify(this.state.payload), Date.now())
+  }
+
+  loadReports() {
+    var data = this.state.coinReport.getReports()
+    console.log(data)
+    this.setState({ text: String(data[0]).split(','), date: String(data[1]).split(','), loadedReports: true })
+  }
+  
+  showReports() {
+    return this.state.text.map((report, i) => {
+      return <div>
+              <p>{ETHEREUM_CLIENT.toAscii(this.state.text[i])}</p>
+              <p>{this.state.date[i]}</p>
+            </div>
+    })
   }
 
   render() {
@@ -26,12 +63,15 @@ class App extends Component {
         <div className='App-header'>
           <img src={logo} />
           <h2>Ducktales</h2>
+          <h4 onClick={this.addReport.bind(this)}>SAVE REPORT</h4>
+          <h4 onClick={this.loadReports.bind(this)}>LOAD REPORTS</h4>
           <div className='graph-key'>
             Don't Buy<div className='red'></div>
            Buy<div className='green'></div>
           </div>
         </div>
         {!this.state.payload._id && <button onClick={this.fetchFromMongo.bind(this)}>NEW CHARTS</button>}
+        {this.state.loadedReports && this.showReports()}
         {this.state.payload._id && <div><div id='usd-graph' className='graph-container'>
           <h1>USD</h1>
           <div className='graph'>
